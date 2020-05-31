87f1bfbf4d003d5d17b0dc5712fb19babe513c63
fltln <mttpr28@gmail.com>
2020-05-31 20:34:24 +0300

    precomputed tables for 0x02 and 0x03 optimizations. x14 performance.

--- lib/sources/aes_encrypt_ecb_128_pure_c.c
+++ lib/sources/aes_encrypt_ecb_128_pure_c.c
@@ -1,7 +1,6 @@
 #include "tcl.h"
 
 #include "aes_defines.h"
-#include "aes_routine_noop.h"
 #include "aes_lookup_tables.h"
 
 void aes_encrypt_ecb_128_pure_c(uint8_t* input, uint8_t* output, uint32_t byteLen, uint8_t* keySchedule)
@@ -15,6 +14,10 @@ void aes_encrypt_ecb_128_pure_c(uint8_t* input, uint8_t* output, uint32_t byteLe
     uint32_t tmpState[NB];
     uint8_t* shiftRowsFromState = (uint8_t*)(state);
     uint8_t* shiftRowsToState = (uint8_t*)(tmpState);
+
+    uint32_t srcColumn;
+    uint8_t* pSrcColumn = (uint8_t*)(&srcColumn);
+    uint8_t* dstColumn;
 
     for(uint32_t i = 0; i < nBlocks; i++)
     {
@@ -58,7 +61,39 @@ void aes_encrypt_ecb_128_pure_c(uint8_t* input, uint8_t* output, uint32_t byteLe
             state[2] = tmpState[2];
             state[3] = tmpState[3];
 
-            mixColumns(state);
+            //mixColumns(state);
+            srcColumn = *(state + 0);
+            dstColumn = (uint8_t*)(state + 0);
+
+            dstColumn[0] = polyMul_0x02(pSrcColumn[0]) ^ polyMul_0x03(pSrcColumn[1]) ^ pSrcColumn[2] ^ pSrcColumn[3];
+            dstColumn[1] = pSrcColumn[0] ^ polyMul_0x02(pSrcColumn[1]) ^ polyMul_0x03(pSrcColumn[2]) ^ pSrcColumn[3];
+            dstColumn[2] = pSrcColumn[0] ^ pSrcColumn[1] ^ polyMul_0x02(pSrcColumn[2]) ^ polyMul_0x03(pSrcColumn[3]);
+            dstColumn[3] = polyMul_0x03(pSrcColumn[0]) ^ pSrcColumn[1] ^ pSrcColumn[2] ^ polyMul_0x02(pSrcColumn[3]);
+
+            srcColumn = *(state + 1);
+            dstColumn = (uint8_t*)(state + 1);
+
+            dstColumn[0] = polyMul_0x02(pSrcColumn[0]) ^ polyMul_0x03(pSrcColumn[1]) ^ pSrcColumn[2] ^ pSrcColumn[3];
+            dstColumn[1] = pSrcColumn[0] ^ polyMul_0x02(pSrcColumn[1]) ^ polyMul_0x03(pSrcColumn[2]) ^ pSrcColumn[3];
+            dstColumn[2] = pSrcColumn[0] ^ pSrcColumn[1] ^ polyMul_0x02(pSrcColumn[2]) ^ polyMul_0x03(pSrcColumn[3]);
+            dstColumn[3] = polyMul_0x03(pSrcColumn[0]) ^ pSrcColumn[1] ^ pSrcColumn[2] ^ polyMul_0x02(pSrcColumn[3]);
+
+            srcColumn = *(state + 2);
+            dstColumn = (uint8_t*)(state + 2);
+
+            dstColumn[0] = polyMul_0x02(pSrcColumn[0]) ^ polyMul_0x03(pSrcColumn[1]) ^ pSrcColumn[2] ^ pSrcColumn[3];
+            dstColumn[1] = pSrcColumn[0] ^ polyMul_0x02(pSrcColumn[1]) ^ polyMul_0x03(pSrcColumn[2]) ^ pSrcColumn[3];
+            dstColumn[2] = pSrcColumn[0] ^ pSrcColumn[1] ^ polyMul_0x02(pSrcColumn[2]) ^ polyMul_0x03(pSrcColumn[3]);
+            dstColumn[3] = polyMul_0x03(pSrcColumn[0]) ^ pSrcColumn[1] ^ pSrcColumn[2] ^ polyMul_0x02(pSrcColumn[3]);
+
+            srcColumn = *(state + 3);
+            dstColumn = (uint8_t*)(state + 3);
+
+            dstColumn[0] = polyMul_0x02(pSrcColumn[0]) ^ polyMul_0x03(pSrcColumn[1]) ^ pSrcColumn[2] ^ pSrcColumn[3];
+            dstColumn[1] = pSrcColumn[0] ^ polyMul_0x02(pSrcColumn[1]) ^ polyMul_0x03(pSrcColumn[2]) ^ pSrcColumn[3];
+            dstColumn[2] = pSrcColumn[0] ^ pSrcColumn[1] ^ polyMul_0x02(pSrcColumn[2]) ^ polyMul_0x03(pSrcColumn[3]);
+            dstColumn[3] = polyMul_0x03(pSrcColumn[0]) ^ pSrcColumn[1] ^ pSrcColumn[2] ^ polyMul_0x02(pSrcColumn[3]);
+            
             //addRoundKey(state, w, round) with unrolled loop.
             state[0] = state[0] ^ w[round * NB + 0];
             state[1] = state[1] ^ w[round * NB + 1];
--- lib/sources/aes_lookup_tables.h
+++ lib/sources/aes_lookup_tables.h
@@ -14,4 +14,8 @@ void subBytes(uint32_t* state);
 // invSubBytes() is the inverse of the subBytes() transformation.
 void invSubBytes(uint32_t* state);
 
+// Polynomial multiplication using precomputed lookup table.
+uint8_t polyMul_0x02(uint8_t value);
+uint8_t polyMul_0x03(uint8_t value);
+
 #endif // __AES_LOOKUP_TABLES_H_
